b378f3bf3a1797f3a878c51ceed6dec58a0e19f7
xinshangshangxin <xinshangshangxin@gmail.com>
2017-03-25 21:21:06 +0800

    chore: add promisifyAll fs typings

--- config/generate-typings.js
+++ config/generate-typings.js
@@ -9,7 +9,7 @@ const autoGeneratePath = path.join(__dirname, 'typings/auto-generate.d.ts');
 function ensureTypingDir() {
   return fs.mkdirAsync(path.join(__dirname, 'typings'))
     .catch((e) => {
-      if(e.code === 'EEXIST') {
+      if (e.code === 'EEXIST') {
         return Promise.resolve();
       }
 
@@ -17,7 +17,7 @@ function ensureTypingDir() {
     });
 }
 
-function getControllerDeclare() {
+function getServicesDeclare() {
   return filePathOneLayer(path.join(serverPath, 'services'))
     .map((serviceFile) => {
       return `declare let ${serviceFile.basename} = require('${serviceFile.basename}');`;
@@ -26,7 +26,6 @@ function getControllerDeclare() {
       return arr.join('\n');
     });
 }
-
 
 function getServiceDeclare() {
   return filePathOneLayer(path.join(serverPath, 'models'))
@@ -61,6 +60,59 @@ function globalLogger() {
   });
 }
 
+function fsPromisifyAll() {
+  return Promise.try(() => {
+    return `
+let Buffer = require('node').Buffer;
+declare let  fs: {
+  renameAsync(oldPath: string, newPath: string): Promise<void>;
+  truncateAsync(path: string | Buffer, len?: number): Promise<void>;
+  ftruncateAsync(fd: number, len?: number): Promise<void>;
+  chownAsync(path: string | Buffer, uid: number, gid: number): Promise<void>;
+  fchownAsync(fd: number, uid: number, gid: number): Promise<void>;
+  lchownAsync(path: string | Buffer, uid: number, gid: number): Promise<void>;
+  chmodAsync(path: string | Buffer, mode: string | number): Promise<void>;
+  fchmodAsync(fd: number, mode: string | number): Promise<void>;
+  lchmodAsync(path: string | Buffer, mode: string | number): Promise<void>;
+  statAsync(path: string | Buffer): Promise<fs.Stats>;
+  lstatAsync(path: string | Buffer): Promise<fs.Stats>;
+  fstatAsync(fd: number): Promise<fs.Stats>;
+  linkAsync(srcpath: string | Buffer, dstpath: string | Buffer): Promise<void>;
+  symlinkAsync(srcpath: string | Buffer, dstpath: string | Buffer, type?: string): Promise<void>;
+  readlinkAsync(path: string | Buffer): Promise<string>;
+  realpathAsync(path: string | Buffer): Promise<string>;
+  unlinkAsync(path: string | Buffer): Promise<void>;
+  rmdirAsync(path: string | Buffer): Promise<void>;
+  mkdirAsync(path: string | Buffer, mode?: string | number): Promise<void>;
+  readdirAsync(path: string | Buffer): Promise<string[]>;
+  closeAsync(fd: number): Promise<void>;
+  openAsync(path: string | Buffer, flags: string | number, mode?: number): Promise<number>;
+  utimesAsync(path: string | Buffer, atime: number | Date, mtime: number | Date): Promise<void>;
+  futimesAsync(fd: number, atime: number | Date, mtime: number | Date): Promise<void>;
+  fsyncAsync(fd: number): Promise<void>;
+  writeAsync(fd: number, data: string, position?: number, encoding?: string): Promise<[number, string]>;
+  writeAsync(fd: number, buffer: Buffer, offset: number, length: number, position?: number): Promise<[number, Buffer]>;
+  readAsync(fd: number, buffer: Buffer, offset: number, length: number, position: number): Promise<[number, Buffer]>;
+  readFileAsync(file: string | number | Buffer, options?:
+             { encoding?: "buffer" | null; flag?: string; }
+             | "buffer"
+             | null): Promise<Buffer>;
+  readFileAsync(file: string | number | Buffer, options: { encoding: string; flag?: string; } | string): Promise<string>;
+  writeFileAsync(file: string | number | Buffer, data: string | Buffer, options?:
+              { encoding?: string | null; mode?: string | number; flag?: string; }
+              | string
+              | null): Promise<void>;
+  appendFileAsync(file: string | number | Buffer, data: string | Buffer, options?:
+               { encoding?: string | null; mode?: number | string; flag?: string; }
+               | string
+               | null): Promise<void>;
+  existsAsync(path: string): Promise<boolean>;
+  accessAsync(path: string, mode?: number): Promise<void>;
+};
+`
+  });
+}
+
 function init() {
   return ensureTypingDir()
     .then(() => {
@@ -68,8 +120,9 @@ function init() {
         .all([
           getGlobal(),
           globalLogger(),
+          fsPromisifyAll(),
           getServiceDeclare(),
-          getControllerDeclare(),
+          getServicesDeclare(),
           getMKoa(),
         ]);
     })
