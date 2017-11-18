833d5cba6670b8d622c7f4384966aa8f202eac70
xinshangshangxin <xinshangshangxin@gmail.com>
2017-11-18 22:08:45 +0800

    fix: n.sh error

--- config/script-tools/n.sh
+++ config/script-tools/n.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-# 载入依赖
-source config/script-tools/constants.sh
+function sourcePrivateEnv() {
+  cd "config/script-tools"
+  local privateEnv=$( ls -a \
+  | grep -E "private-.*\.sh" \
+  | grep -v "private-env.default.sh")
+
+  echo "privateEnv:"${privateEnv}
+
+  declare -a arr=(${privateEnv})
+
+  for word in ${arr[@]}
+  do
+      source ${word}
+  done
+}
+
+# 先载入私有环境
+sourcePrivateEnv
 
 function n() {
   now $* -t ${nowToken}
